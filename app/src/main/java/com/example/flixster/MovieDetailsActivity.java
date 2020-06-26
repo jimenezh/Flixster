58a9169c2013dfab03c1bbd3328d85decac7fd7f
jimenezh <jimenezshr@fb.com>
2020-06-26 11:46:59 -0700

    Added last binding

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.example.flixster.databinding.ActivityMovieDetailsBinding;
+import com.example.flixster.databinding.ActivityMovieTrailerBinding;
 import com.example.flixster.models.Movie;
 
 import org.parceler.Parcel;
@@ -30,21 +32,20 @@ public class MovieDetailsActivity extends AppCompatActivity {
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
         Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
 
-       tvTitle = findViewById(R.id.tvTitle);
-       tvOverview = findViewById(R.id.tvOverview);
-       rbVoteAverage = (RatingBar) findViewById(R.id.tvRating);
+        ActivityMovieDetailsBinding binding = ActivityMovieDetailsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         // unwrap the movie passed in via intent, using its simple name as a key
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
         Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
 
         // set the title and overview
-        tvTitle.setText(movie.getTitle());
-        tvOverview.setText(movie.getOverview());
+        binding.tvTitle.setText(movie.getTitle());
+        binding.tvOverview.setText(movie.getOverview());
 
         // vote average is 0..10, convert to 0..5 by dividing by 2
         float voteAverage = (float) movie.getVoteAverage();
-        rbVoteAverage.setRating(voteAverage = voteAverage > 0 ? voteAverage / 2.0f : voteAverage);
+        binding.tvRating.setRating(voteAverage = voteAverage > 0 ? voteAverage / 2.0f : voteAverage);
 
 
     }
